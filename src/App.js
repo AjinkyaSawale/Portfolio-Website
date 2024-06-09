033cc67764c60b98cd2b40b8ded0f8fffbab419a
AjinkyaSawale <ajinkyasawale.work@gmail.com>
2024-06-09 11:10:54 +0530

    Modified Navbar

--- src/App.js
+++ src/App.js
@@ -5,28 +5,28 @@ import { Home } from "./Home";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { ContactMe } from "./components/ContactMe";
-//import { Footer } from "./Footer";
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/Portfolio-Website">
       <div className="App">
         <MyNavbar />
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/Home" element={<Home />} />
           <Route path="/Skills" element={<Skills />} />
           <Route path="/Projects" element={<Projects />} />
           <Route path="/ContactMe" element={<ContactMe />} />
         </Routes>
-       
       </div>
     </Router>
   );
 }
 
 export default App;
+
 
 
 
--- src/components/MyNavbar.js
+++ src/components/MyNavbar.js
@@ -27,72 +27,70 @@ const MyNavbar = () => {
   };
 
   return (
-    <>
-      <BootstrapNavbar expand="lg" className={scrolled ? "scrolled" : ""}>
-        <Container>
-          <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav">
-            <span className="navbar-toggler-icon"></span>
-          </BootstrapNavbar.Toggle>
-          <BootstrapNavbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link
-                as={Link}
-                to="/Home"
-                className={activeLink === "home" ? "active navbar-link" : "navbar-link"}
-                onClick={() => onUpdateLink("home")}
-              >
-                Home
-              </Nav.Link>
-              <Nav.Link
-                as={Link}
-                to="/Skills"
-                className={activeLink === "skills" ? "active navbar-link" : "navbar-link"}
-                onClick={() => onUpdateLink("skills")}
-              >
-                Skills
-              </Nav.Link>
-              <Nav.Link
-                as={Link}
-                to="/Projects"
-                className={activeLink === "projects" ? "active navbar-link" : "navbar-link"}
-                onClick={() => onUpdateLink("projects")}
-              >
-                Projects
-              </Nav.Link>
-              <Nav.Link
-                as={Link}
-                to="/ContactMe"
-                className={activeLink === "contactme" ? "active navbar-link" : "navbar-link"}
-                onClick={() => onUpdateLink("contactme")}
-              >
-                Contact Me
-              </Nav.Link>
-            </Nav>
-            <span className="navbar-text">
-              <div className="social-icon">
-                <a href="https://www.linkedin.com/in/ajinkya-sawale/">
-                  <img src={navIcon1} alt="linkedln-img"></img>
-                </a>
-                <a href="https://github.com/AjinkyaSawale">
-                  <img src={navIcon2} alt="github-img"></img>
-                </a>
-                <a href="mailto:ajinkyasawale.work@gmail.com">
-                  <img src={navIcon3} alt="email-img"></img>
-                </a>
-              </div>
-              <button
-                className="vvd"
-                onClick={() => {
-                  console.log("connect");
-                }}
-              >
-                <span>Let's Connect</span>
-              </button>
-            </span>
-          </BootstrapNavbar.Collapse>
-        </Container>
-      </BootstrapNavbar>
-    </>
+    <BootstrapNavbar expand="lg" className={scrolled ? "scrolled" : ""}>
+      <Container>
+        <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav">
+          <span className="navbar-toggler-icon"></span>
+        </BootstrapNavbar.Toggle>
+        <BootstrapNavbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link
+              as={Link}
+              to="/"
+              className={activeLink === "home" ? "active navbar-link" : "navbar-link"}
+              onClick={() => onUpdateLink("home")}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to="/Skills"
+              className={activeLink === "skills" ? "active navbar-link" : "navbar-link"}
+              onClick={() => onUpdateLink("skills")}
+            >
+              Skills
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to="/Projects"
+              className={activeLink === "projects" ? "active navbar-link" : "navbar-link"}
+              onClick={() => onUpdateLink("projects")}
+            >
+              Projects
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to="/ContactMe"
+              className={activeLink === "contactme" ? "active navbar-link" : "navbar-link"}
+              onClick={() => onUpdateLink("contactme")}
+            >
+              Contact Me
+            </Nav.Link>
+          </Nav>
+          <span className="navbar-text">
+            <div className="social-icon">
+              <a href="https://www.linkedin.com/in/ajinkya-sawale/">
+                <img src={navIcon1} alt="linkedln-img" />
+              </a>
+              <a href="https://github.com/AjinkyaSawale">
+                <img src={navIcon2} alt="github-img" />
+              </a>
+              <a href="mailto:ajinkyasawale.work@gmail.com">
+                <img src={navIcon3} alt="email-img" />
+              </a>
+            </div>
+            <button
+              className="vvd"
+              onClick={() => {
+                console.log("connect");
+              }}
+            >
+              <span>Let's Connect</span>
+            </button>
+          </span>
+        </BootstrapNavbar.Collapse>
+      </Container>
+    </BootstrapNavbar>
   );
 };
 
